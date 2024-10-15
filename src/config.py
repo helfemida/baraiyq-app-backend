92b8bac542348d0bba5602171e65e683be110bfe
Albina Yermek <yermekalbina@gmail.com>
2024-10-15 16:31:39 +0500

    prod deploy version

--- src/config.py
+++ src/config.py
@@ -4,8 +4,9 @@ DB_USER = "postgres"
 DB_PASS = os.getenv("DB_PASS", "postgres123")
 DB_NAME = "baraiyq-db" 
 DB_CONNECTION_NAME = "baraiyq-app-438307:us-central1:baraiyq-db"  
+DB_IP = "34.68.21.214"
 
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@/{DB_NAME}?host=/cloudsql/{DB_CONNECTION_NAME}"
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_IP}/{DB_NAME}"
 
 #DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@localhost/{DB_NAME}"
 
