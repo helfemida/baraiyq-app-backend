c340b29e520acf145cdc5751d1a33292882d3325
Your Name <you@example.com>
2024-11-01 01:04:05 +0500

    some changes

--- src/schemas/order_schemas.py
+++ src/schemas/order_schemas.py
@@ -1,14 +1,18 @@
 from pydantic import BaseModel
 from typing import List
 
+class TimeSlot(BaseModel):
+    office_id: int
+    start_time: str
+    end_time: str
+
 class OrderRequest(BaseModel):
     client_id: int
     office_ids: List[int]
     total_price: float
     people_amount: int
     date: str
-    start_time: str
-    end_time: str
+    time_slots: List[TimeSlot]
 
 class OrderResponse(BaseModel):
     id: int
@@ -17,8 +21,7 @@ class OrderResponse(BaseModel):
     total_price: float
     people_amount: int
     date: str
-    start_time: str
-    end_time: str
+    time_slots: List[TimeSlot]
 
     class Config:
         orm_mode = True
